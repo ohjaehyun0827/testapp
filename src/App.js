01c17274a89d987b7ee697f845e258686e1f8c4d
ohjaehyun0827 <phantom030827@gmail.com>
2025-11-10 10:43:22 +0900

    1110

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello.js';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          REACT STUDY! <code>src/App.js</code>
+          <Hello />
         </p>
         <a
           className="App-link"
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import './Hello.css';
+
+function Hello(props){
+    return (
+        <h1 class="ak">Hello World</h1>
+    );
+}
+export default Hello;
